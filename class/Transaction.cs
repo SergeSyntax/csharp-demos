b323756bc065bfd7feab1d848adffaf0c9f4fecc
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-11-12 01:44:46 +0200

    created class demo

---
+++ class/Transaction.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace classes {
+  public class Transaction {
+    public decimal Amount {get;}
+    public DateTime Date {get;}
+    public string Note {get;}
+
+    public Transaction(decimal amount, DateTime date, string note) {
+      this.Amount = amount;
+      this.Date = date;
+      this.Note = note;
+    }
+  }
+}
